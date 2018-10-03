1c47131a05776fa0930e99b20667f9569e730c13
Jared Griffin <griffinjared1388@gmail.com>
2018-10-03 11:43:57 -0500

    Began to format time displayed

--- frontend/src/components/Alarm/Alarm.js
+++ frontend/src/components/Alarm/Alarm.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-export default () => (
-    <div>test</div>
+export default ({ children }) => (
+    <Paper>
+        <Typography>
+            {children} AM
+        </Typography>
+    </Paper>
 );
---
+++ frontend/src/components/Alarm/Alarm.test.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Chance from 'chance';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Alarm from './Alarm';
+
+describe('Alarm', () => {
+    let wrapper;
+    let chance;
+
+    const generateFakeTime = (options = {}) => {
+        const { isAM } = options;
+
+        const convertToTwoDigitNumber = number => number.length === 1 ? `0${number}` : number;
+    
+        const ampmOffset = isAM ? 0 : 12;
+        const randomHour = convertToTwoDigitNumber(`${chance.hour() + ampmOffset - 1}`);
+        const randomMinute = convertToTwoDigitNumber(`${chance.minute()}`);
+    
+        const randomTime = `${randomHour}:${randomMinute}`;
+
+        return randomTime;
+      }
+
+    const renderComponent = (props = {}) => {
+        const { children } = props;
+
+        return shallow(<Alarm>{children || generateFakeTime()}</Alarm>)
+    }
+
+    beforeEach(() => {
+        chance = Chance();
+
+        wrapper = renderComponent();
+    });
+
+    it('is wrapped in a Paper component', () => {
+        expect(wrapper.type()).toEqual(Paper);
+    });
+
+    describe('Time', () => {
+        let time;
+
+        beforeEach(() => {
+            time = wrapper.childAt(0);
+        });
+
+        it('is a Typography component', () => {
+            expect(time.type()).toEqual(Typography);
+        });
+
+        it('displays the child passed in to the Alarm component correctly when AM', () => {
+            const children = generateFakeTime({ isAM: true });
+
+            wrapper = renderComponent({ children });
+            time = wrapper.childAt(0);
+
+            const timeText = time.childAt(0).text();
+
+            expect(timeText).toEqual(`${children} AM`);
+        });
+
+        it('displays the child passed in to the Alarm component correctly when PM', () => {
+            const children = generateFakeTime({ isAM: false });
+
+            wrapper = renderComponent({ children });
+            time = wrapper.childAt(0);
+
+            const timeText = time.childAt(0).text();
+
+            const offsetTime = children
+                .split(':')
+                .map((timeComponent, index) => {
+                    if (index === 0) {
+                        const shiftedTime = `${parseInt(timeComponent) - 12}`;
+                        return shiftedTime.length === 1 ? `0${shiftedTime}` : shiftedTime;
+                    }
+
+                    return timeComponent;
+                })
+                .join(':');
+
+            expect(timeText).toEqual(`${offsetTime} PM`);
+        });
+    });
+});
